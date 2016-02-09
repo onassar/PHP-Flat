77d0685f095e3f33fe77a156d6001dd0c921d5b9
Oliver Nassar <onassar@gmail.com>
2016-02-09 18:45:10 -0500

    Init

---
+++ Flat.class.php
@@ -0,0 +1,59 @@
+<?php
+
+    /**
+     * Flat
+     *
+     * @link    https://github.com/onassar/PHP-Flat
+     * @author  Oliver Nassar <onassar@gmail.com>
+     */
+    class Flat extends stdClass
+    {
+        /**
+         * _path
+         * 
+         * @var    string|false (default: false)
+         * @access protected
+         */
+        protected $_path = false;
+
+        /**
+         * __construct
+         * 
+         * @access public
+         * @param  string $path
+         * @return void
+         */
+        public function __construct($path)
+        {
+            $this->_path = $path;
+            $contents = file_get_contents($this->_path);
+            $data = json_decode($contents);
+            $this->_setup($data);
+        }
+
+        /**
+         * _setup
+         * 
+         * @access protected
+         * @param  stdClass $data
+         * @return void
+         */
+        public function _setup($data)
+        {
+            foreach ($data as $key => $value) {
+                $this->{$key} = $value;
+            }
+        }
+
+        /**
+         * save
+         * 
+         * @access public
+         * @return void
+         */
+        public function save()
+        {
+            $encoded = json_encode($this);
+            file_put_contents($this->_path, $encoded);
+        }
+    }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# PHP-Flat
+PHP-Flat
+===
+
+### Example of reading
+
+``` php
+<?php
+    require_once APP . '/vendors/PHP-Flat/Flat.class.php';
+    $flat = Flat::read();
+    exit(0);
+
+```
